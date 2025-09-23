11d20f935a60b6db6a98fb35fb13e8e791eb161a
Lennonpdc <lennonpaulcabrera@gmail.com>
2025-09-23 16:20:23 +0800

    feat: added My Contact component

--- components/MyFooter.vue
+++ components/MyFooter.vue
@@ -1,15 +1,7 @@
 <template>
-    <footer class="bg-gray-900 text-white py-6 mt-20">
+    <footer class="bg-blue-950 text-white py-6 mt-20">
         <div class="container mx-auto flex justify-center gap-6">
-            <!-- Facebook -->
-            <!-- <a href="https://facebook.com/yourprofile" target="_blank" aria-label="Facebook">
-                <svg class="w-6 h-6 hover:text-blue-400" fill="currentColor" viewBox="0 0 24 24">
-                    <path
-                        d="M22 12a10 10 0 1 0-11.5 9.9v-7h-2v-3h2v-2.3c0-2 1.2-3.1 3-3.1.9 0 1.8.2 1.8.2v2h-1c-1 0-1.3.6-1.3 1.2V12h2.3l-.4 3h-1.9v7A10 10 0 0 0 22 12z" />
-                </svg>
-            </a> -->
 
-            <!-- LinkedIn -->
             <a href="https://www.linkedin.com/in/lennonpauldcabrera/" target="_blank" aria-label="LinkedIn">
                 <svg class="w-6 h-6 hover:text-blue-300" fill="currentColor" viewBox="0 0 24 24">
                     <path d="M19 3A2 2 0 0 1 21 5v14a2 2 0 0 1-2 2H5a2 
@@ -20,7 +12,6 @@
                 </svg>
             </a>
 
-            <!-- GitHub -->
             <a href="https://github.com/yourprofile" target="_blank" aria-label="GitHub">
                 <svg class="w-6 h-6 hover:text-gray-400" fill="currentColor" viewBox="0 0 24 24">
                     <path d="M12 .3a12 12 0 0 0-3.8 23.4c.6.1.8-.3.8-.6v-2
@@ -31,25 +22,13 @@
                 </svg>
             </a>
 
-            <!-- Gmail -->
-            <a href="mailto:your.email@gmail.com" aria-label="Gmail">
+            <a href="mailto:lennonpaulcabrera@gmail.com" aria-label="Gmail">
                 <svg class="w-6 h-6 hover:text-red-400" fill="currentColor" viewBox="0 0 24 24">
                     <path d="M12 13 2 6.76V18h20V6.76L12 13zm10-7.75L12 12 
                    2 5.25V4h20v1.25z" />
                 </svg>
             </a>
 
-            <!-- Instagram -->
-            <!-- <a href="https://instagram.com/yourprofile" target="_blank" aria-label="Instagram">
-                <svg class="w-6 h-6 hover:text-pink-400" fill="currentColor" viewBox="0 0 24 24">
-                    <path d="M7 2C4.2 2 2 4.2 2 7v10c0 2.8 2.2 5 5 5h10
-                   c2.8 0 5-2.2 5-5V7c0-2.8-2.2-5-5-5H7zm10 2c1.7 0 
-                   3 1.3 3 3v10c0 1.7-1.3 3-3 3H7c-1.7 0-3-1.3-3-3V7
-                   c0-1.7 1.3-3 3-3h10zm-5 3a5 5 0 1 0 0 10 5 5 0 
-                   0 0 0-10zm0 2a3 3 0 1 1 0 6 3 3 0 0 1 0-6zm4.5-2
-                   a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3z" />
-                </svg>
-            </a> -->
         </div>
         <div class="container mx-auto flex justify-center gap-6 mt-2">
             © 2025 Lennon Paul Cabrera
